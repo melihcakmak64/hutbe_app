4c08b32d768b3045de04f23ff1cab36e54527e31
Melih Çakmak <melihcakmak1755@gmail.com>
2025-08-07 12:54:55 +0300

    refactoring

--- app/src/main/java/com/example/hutbe/view/screens/HutbeDetailScreen.kt
+++ app/src/main/java/com/example/hutbe/view/screens/HutbeDetailScreen.kt
@@ -4,6 +4,7 @@ import PdfViewer
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
@@ -49,7 +50,7 @@ fun HutbeDetailScreen(
                 navigationIcon = {
                     IconButton(onClick = onNavigateBack) {
                         Icon(
-                            Icons.Default.ArrowBack,
+                            Icons.AutoMirrored.Filled.ArrowBack,
                             contentDescription = "Geri",
                             tint = Color.White
                         )
